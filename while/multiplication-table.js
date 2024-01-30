6d9cf5d16e568fd09ca38322c397bf77c0a2aef8
abunaim5 <abunaim140@gmail.com>
2024-01-30 20:32:02 +0600

    multiplication table for 5 problem solved

---
+++ while/multiplication-table.js
@@ -0,0 +1,8 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+let i = 1;
+let n = 5;
+while (i <= 10) {
+    console.log(n + '*' + i + '=' + n * i);
+    i++
+    let getTable = n * i;
+}
