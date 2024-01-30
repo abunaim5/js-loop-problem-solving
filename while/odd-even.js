659bc548afba572b6695ceb8489bdfb27a836d16
abunaim5 <abunaim140@gmail.com>
2024-01-30 19:19:08 +0600

    odd and even numbers problem solved

---
+++ while/odd-even.js
@@ -0,0 +1,17 @@
+// Find all the odd numbers from 41 to 100
+let i = 41;
+while (i <= 100) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+// Find all the even numbers from 41 to 100
+// let i = 41;
+while (i <= 100) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    i++;
+}
